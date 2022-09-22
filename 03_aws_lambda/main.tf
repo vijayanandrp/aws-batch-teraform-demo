8008e5bb3599eef750fe82b4ddd47e4761bff523
root <root@ip-172-31-25-26.ec2.internal>
2022-09-22 12:48:24 +0000

    Added Lambda

--- 03_aws_lambda/main.tf
+++ 03_aws_lambda/main.tf
@@ -1,8 +1,26 @@
-locals {
-  prefix   = "managing-alb-using-terraform"
+provider "aws" {
+  region = "us-east-1"
+}
 
-  common_tags = {
-    Environment = "dev"
-    Project     = "hands-on.cloud"
-  }
+resource "random_pet" "this" {
+  length = 2
+}
+
+
+module "lambda_function" {
+  source = "terraform-aws-modules/lambda/aws"
+  version = "v4.0.2"
+
+  publish = true
+
+  function_name = "${random_pet.this.id}-lambda-simple-demo"
+  handler       = "index.lambda_handler"
+  runtime       = "python3.8"
+
+  attach_policies = true
+  policies = ["arn:aws:iam::aws:policy/AWSBatchFullAccess"]
+  
+  source_path = [
+    "${path.module}/src/index.py",
+  ]
 }
