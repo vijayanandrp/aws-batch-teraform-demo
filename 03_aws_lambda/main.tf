7d4e89fdd711c8686a73a3958db158dc1b2d1c9c
Vijay Anand Pandian <vijayanandrp@gmail.com>
2022-09-24 18:51:47 -0400

    Update main.tf

--- 03_aws_lambda/main.tf
+++ 03_aws_lambda/main.tf
@@ -6,6 +6,23 @@ resource "random_pet" "this" {
   length = 2
 }
 
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda" {
+  name                  = "EtlLambdaRole"
+  assume_role_policy    = data.aws_iam_policy_document.assume_role.json
+}
+
 
 module "lambda_function" {
   source = "terraform-aws-modules/lambda/aws"
@@ -14,8 +31,11 @@ module "lambda_function" {
   publish = true
 
   function_name = "${random_pet.this.id}-lambda-batch-simple-demo"
+  description   = "Trigger the AWS Batch from Lambda Function"
   handler       = "index.lambda_handler"
-  runtime       = "python3.8"
+  runtime       = "python3.9"
+  role          = aws_iam_role.iam_for_lambda.arn
+  publish       = true
 
   attach_policies = true
   policies = ["arn:aws:iam::aws:policy/AWSBatchFullAccess"]
@@ -23,4 +43,7 @@ module "lambda_function" {
   source_path = [
     "${path.module}/src/index.py",
   ]
+  
+  create_role = false
+  lambda_role = aws_iam_role.lambda.arn
 }
