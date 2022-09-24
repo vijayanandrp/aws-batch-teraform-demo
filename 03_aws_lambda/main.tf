c49191f57c28d9700f6d83aa081ed9f16b747e4a
Vijay Anand Pandian <vijayanandrp@gmail.com>
2022-09-24 19:18:30 -0400

    Update main.tf

--- 03_aws_lambda/main.tf
+++ 03_aws_lambda/main.tf
@@ -35,7 +35,6 @@ module "lambda_function" {
   handler       = "index.lambda_handler"
   runtime       = "python3.9"
   role          = aws_iam_role.iam_for_lambda.arn
-  publish       = true
 
   attach_policies = true
   policies = ["arn:aws:iam::aws:policy/AWSBatchFullAccess"]
