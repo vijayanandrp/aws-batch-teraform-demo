f85463241272e2d495830a245241381920283b55
VijayAnandRP <vijayanandrp@gmail.com>
2022-09-24 19:09:08 +0100

    renamed the batch name

--- 02_aws_fargate/main.tf
+++ 02_aws_fargate/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 locals {
   region = "us-east-1"
-  name   = "batch-s3-file-encrypt-decrypt"
+  name   = "batch-s3-file-encrypt-decrypt-service"
 
   tags = {
     Name       = local.name
