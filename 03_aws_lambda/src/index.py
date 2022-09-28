dc5a95203fd7d0c82be2eca5af2ed96a009676a8
Vijay Anand Pandian <vijayanandrp@gmail.com>
2022-09-28 17:17:29 +0100

    Update index.py

--- 03_aws_lambda/src/index.py
+++ 03_aws_lambda/src/index.py
@@ -1,27 +1,56 @@
 import boto3
+import os
+import logging
+import random
+import string
 
-client = boto3.client('batch')
+file_name = os.path.splitext(os.path.basename(__file__))[0]
+
+default_log_args = {
+    "level": logging.INFO,
+    "format": "%(asctime)s [%(levelname)s] %(name)s - %(message)s",
+    "datefmt": "%d-%b-%y %H:%M"
+}
+
+root = logging.getLogger()
+if root.handlers:
+    for handler in root.handlers:
+        root.removeHandler(handler)
+
+
+def get_logger(name):
+    logging.basicConfig(**default_log_args)
+    return logging.getLogger(name)
+
 
 def get_random_string(length):
-    # With combination of lower and upper case
-    result_str = ''.join(random.choice(string.ascii_letters) for i in range(length))
-    # print random string
-    print(result_str)
+    return ''.join(random.choice(string.ascii_letters) for i in range(length))
 
-def lambda_handler(event, context):
-    job_name='demo_lambda_encrypt_batch_' + get_random_string(8)
-    print('job_name', job_name)
-    
-    
-    response = client.submit_job(
-    jobDefinition='batch-ex-fargate:2',
-    jobQueue='HighPriorityFargate',
-    jobName=job_name,
-    shareIdentifier='A1*',
-    schedulingPriorityOverride=0,
-    containerOverrides={
-        'command': ["file_crypto_service.bash", "60"],
-        'environment': [
+
+def lambda_handler(event: dict = None, context: dict = None):
+    client = boto3.client('batch')
+    log = get_logger(f"{file_name}.{lambda_handler.__name__}")
+    log.info('=' * 30 + " Init " + '=' * 30)
+    job_name = 'demo_lambda_encrypt_batch_' + get_random_string(8)
+    log.info(f"job_name - {job_name}")
+    log.info(f"event - {event}")
+    log.info(f"context - {context}")
+    records = event.get("Records", None)
+    if not records:
+        log.info(f"[-] No Records found in the events - {event}")
+        return None
+
+    new_files = [{'bucket': record["s3"]["bucket"]["name"],
+                  'key': record['s3']['object']['key'],  # m5/insights/new_user_report/date=2022-05-22
+                  'size': record['s3']['object']['size']}
+                 for record in records
+                 if record.get("s3")]
+
+    for new_file in new_files:
+        log.info(f'New File - {new_file}')
+        bucket = new_file['bucket']
+        file_key = new_file['key']
+        environment = [
             {
                 'name': 'BATCH_FILE_S3_URL',
                 'value': 's3://s3-encrypt-demo-batch/file_crypto_service.bash'
@@ -32,7 +61,7 @@ def lambda_handler(event, context):
             },
             {
                 'name': 'ENV_SOURCE_BUCKET',
-                'value': 's3-encrypt-demo-batch'
+                'value': bucket
             },
             {
                 'name': 'ENV_TARGET_BUCKET',
@@ -40,7 +69,7 @@ def lambda_handler(event, context):
             },
             {
                 'name': 'ENV_FILE_KEY',
-                'value': 'testData.csv'
+                'value': file_key
             },
             {
                 'name': 'ENV_IS_ENCRYPT',
@@ -55,11 +84,22 @@ def lambda_handler(event, context):
                 'value': 's3://s3-encrypt-demo-batch/symmetric_keyfile.key'
             }
         ]
-        },
-       timeout={
-        'attemptDurationSeconds': 3000
-    },
-    )
+        log.info(f"environment - {environment}")
 
-    print(response)
-    return {'jobName': job_name}
+        response = client.submit_job(
+            jobDefinition='batch-ex-fargate:2',
+            jobQueue='HighPriorityFargate',
+            jobName=job_name,
+            shareIdentifier='A1*',
+            schedulingPriorityOverride=0,
+            containerOverrides={
+                'command': ["file_crypto_service.bash", "60"],
+                'environment': environment
+            },
+            timeout={
+                'attemptDurationSeconds': 3000
+            },
+        )
+
+        log.info(response)
+        return {'jobName': job_name, 'environment': environment}
