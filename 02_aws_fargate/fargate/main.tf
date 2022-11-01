9da5b23d2e9daa965aa9d10ff3595395223d0004
VPandian <vpandina@channel4.co.uk>
2022-11-01 19:44:10 +0000

    fix6

--- 02_aws_fargate/fargate/main.tf
+++ 02_aws_fargate/fargate/main.tf
@@ -16,7 +16,7 @@ locals {
 
 data "aws_region" "current" {}
 
-data "aws_subnets" "example" {
+data "aws_subnet" "example" {
   filter {
     name   = "vpc-id"
     values = [local.vpc_id]
@@ -24,7 +24,7 @@ data "aws_subnets" "example" {
 }
 
 data "aws_subnet" "example" {
-  for_each = toset(data.aws_subnets.example.ids)
+  for_each = toset(data.aws_subnet.example.ids)
   id       = each.value
 }
 
@@ -81,7 +81,7 @@ module "batch" {
         max_vcpus = 8
 
         security_group_ids = [module.vpc_efs_security_group.security_group_id, module.vpc_batch_security_group.security_group_id]
-        subnets            =   aws_subnets.batch_subnet.ids
+        subnets            =   aws_subnet.batch_subnet.ids
 
         # `tags = {}` here is not applicable for spot
       }
@@ -95,7 +95,7 @@ module "batch" {
         max_vcpus = 8
 
         security_group_ids = [module.vpc_efs_security_group.security_group_id, module.vpc_batch_security_group.security_group_id]
-        subnets            =  aws_subnets.batch_subnet.ids
+        subnets            =  aws_subnet.batch_subnet.ids
 
         # `tags = {}` here is not applicable for spot
       }
@@ -401,7 +401,7 @@ resource "aws_efs_access_point" "test" {
 # AWS EFS Mount point uses File system ID to launch.
 resource "aws_efs_mount_target" "mount" {
     file_system_id  = aws_efs_file_system.efs.id
-    count           = length(aws_subnets.batch_subnet.ids)
-    subnet_id       = tolist(aws_subnets.batch_subnet.ids)[count.index]
+    count           = length(aws_subnet.batch_subnet.ids)
+    subnet_id       = tolist(aws_subnet.batch_subnet.ids)[count.index]
     security_groups = [module.vpc_efs_security_group.security_group_id, module.vpc_batch_security_group.security_group_id]
 }
